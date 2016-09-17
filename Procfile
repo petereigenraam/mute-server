73f78edd4f1598ae3441750390abfc9a8621a285
Peter Eigenraam <peter.eigenraam@wearespindle.com>
2016-09-17 17:47:46 +0200

    Add initial settings for Heroku, and a gitignore

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --config=gunicorn.py mute.wsgi:application
--- mute/settings.py
+++ mute/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -81,12 +83,8 @@ WSGI_APPLICATION = 'mute.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default='sqlite:///%s' % os.path.join(BASE_DIR, 'db.sqlite3')),
 }
 
 
