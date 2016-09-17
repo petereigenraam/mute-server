2ed710c68114de1bc25d9f3343debcc95c5c49f3
Peter Eigenraam <peter.eigenraam@wearespindle.com>
2016-09-17 18:03:43 +0200

    Remove the reference to static folder

--- mute/settings.py
+++ mute/settings.py
@@ -128,6 +128,5 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
